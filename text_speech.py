bf30580946058dcef05a7311025a7142ee6d008d
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-12-29 21:51:22 +0530

    'new'

--- text_speech.py
+++ text_speech.py
@@ -1,10 +1,12 @@
 import pyttsx3
 
 engine = pyttsx3.init()
-
-
+voice=engine.getProperty('voices')
+engine.setProperty('voice',voice[1].id)
+newVoiceRate = 150
+engine.setProperty('rate', newVoiceRate)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-speak("This is Friday")
+speak("Hello! This is python assistant")
